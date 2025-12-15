7ad2d1d92773cff1dde808e9484c13a7eb962ae2
Purva Ekka <gamerpurva@gmail.com>
2025-12-15 09:26:53 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-2040456421-d36d0",
+  "appId": "1:743363426858:web:1d1727b57cb330d4536153",
+  "apiKey": "AIzaSyDBGczIs1vne9yWslyn73I2ZlAEiCwA_5A",
+  "authDomain": "studio-2040456421-d36d0.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "743363426858"
+};
